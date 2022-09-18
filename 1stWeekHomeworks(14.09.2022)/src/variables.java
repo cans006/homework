6707150a4395bceaf90d686b333f33af75d7cea2
can <can@localhost>
2022-09-18 18:03:46 +0300

    second

---
+++ 1stWeekHomeworks(14.09.2022)/src/variables.java
@@ -0,0 +1,12 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayisi = 10;
+		String mesaj = "Öğrenci Sayısı: ";
+		
+		System.out.println(mesaj+ogrenciSayisi);
+	}
+
+}
